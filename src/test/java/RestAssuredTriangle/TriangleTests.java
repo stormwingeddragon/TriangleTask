2da6b4685c1f4b6324ed345c66e1c45a5b5ebfae
stormwingeddragon <stormwingeddragon@gmail.com>
2020-01-29 22:40:41 +0300

    tests added

--- src/test/java/RestAssuredTriangle/TriangleTests.java
+++ src/test/java/RestAssuredTriangle/TriangleTests.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import javax.swing.text.Utilities;
 
 import static RestAssuredTriangle.Utilities.*;
+import static RestAssuredTriangle.Utilities.getFirstExistingTriangle;
 
 public class TriangleTests {
     private Utilities u;
@@ -21,7 +22,7 @@ public class TriangleTests {
     public void checkTriangleArea() throws Exception {
         String id = createTriangle("{\"separator\": \";\", \"input\": \"3;4;5\"}");
         float area = calculateArea(id);
-        Assert.assertEquals(area, 6.0);
+        Assert.assertEquals(area, 6.0, 0.0001);
         deleteTriangle(id);
     }
 
@@ -39,6 +40,38 @@ public class TriangleTests {
         deleteTriangle(getFirstExistingTriangle());
     }
 
+    //Ignores fourth side
+    @Ignore
+    @Test
+    public void fourSides() throws Exception {
+        createTriangleAndCheckResponse("{\"separator\": \";\", \"input\": \"3;4;5;4\"}", 422);
+        deleteTriangle(getFirstExistingTriangle());
+    }
+
+    @Test
+    public void twoSides() throws Exception {
+        createTriangleAndCheckResponse("{\"separator\": \";\", \"input\": \"3;4\"}", 422);
+        deleteTriangle(getFirstExistingTriangle());
+    }
+
+    @Test
+    public void wrongSeparator() throws Exception {
+        createTriangleAndCheckResponse("{\"separator\": \",\", \"input\": \"3;4;5\"}", 422);
+        deleteTriangle(getFirstExistingTriangle());
+    }
+
+    @Test
+    public void noSeparatorSpecified() throws Exception {
+        createTriangleAndCheckResponse("{\"input\": \"3;4;5\"}", 200);
+        deleteTriangle(getFirstExistingTriangle());
+    }
+
+    @Test
+    public void noSeparatorSpecifiedCommaUsed() throws Exception {
+        createTriangleAndCheckResponse("{\"input\": \"3,4,5\"}", 422);
+        deleteTriangle(getFirstExistingTriangle());
+    }
+
     @Test
     public void negativeSidesLengthsDoNotCauseException() throws Exception {
         createTriangleAndCheckResponse("{\"separator\": \";\", \"input\": \"-3;-4;-5\"}", 200);
@@ -51,6 +84,7 @@ public class TriangleTests {
         deleteTriangle(getFirstExistingTriangle());
     }
 
+    //Bug found, possible to create 11 triangles instead of 10
     @Test
     @Ignore
     public void checkImpossibleToCreateEleventhTriangle() throws Exception {
